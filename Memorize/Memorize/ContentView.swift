fb0a509f52fa2f0e0dbedba3454dcb9c75ac151b
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2025-12-10 09:28:38 +0100

    extract card-view into its own structure to avoid repitition of code

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -9,6 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        HStack {
+            CardView()
+            CardView()
+            CardView()
+            CardView()
+        }
+        .padding()
+        .foregroundStyle(.orange)
+    }
+}
+
+struct CardView: View {
+    var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 12)
                 .foregroundStyle(.white)
@@ -16,8 +29,6 @@ struct ContentView: View {
                 .strokeBorder(lineWidth: 2)
             Text("👻").font(.largeTitle)
         }
-        .padding()
-        .foregroundStyle(.orange)
     }
 }
 
