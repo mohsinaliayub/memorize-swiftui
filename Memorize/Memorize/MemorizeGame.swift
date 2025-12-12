5f99c5c8791a5a3d7fa89ff28e34cd9c78a605e0
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2025-12-12 08:34:38 +0100

    create a stub for our game model

---
+++ Memorize/Memorize/MemorizeGame.swift
@@ -0,0 +1,22 @@
+//
+//  MemorizeGame.swift
+//  Memorize
+//
+//  Created by Mohsin Ali Ayub on 12.12.25.
+//
+
+import Foundation
+
+struct MemoryGame<CardContent> {
+    var cards: Array<Card>
+    
+    func choose(_ card: Card) {
+        
+    }
+    
+    struct Card {
+        var isFaceUp: Bool
+        var isMatched: Bool
+        let content: CardContent
+    }
+}
