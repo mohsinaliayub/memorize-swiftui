7e3eb957bc321384839f47997073611b2918122e
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2025-12-12 08:39:13 +0100

    create a stub for view model

---
+++ Memorize/Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,13 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Mohsin Ali Ayub on 12.12.25.
+//
+
+import Foundation
+
+class EmojiMemoryGame {
+    // TODO: Finish the implementation
+    var model = MemoryGame<String>(cards: [])
+}
