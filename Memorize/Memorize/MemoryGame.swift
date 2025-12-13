f651d0151633f4369da2e451c6ade9e44be65e1f
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2025-12-13 10:46:57 +0100

    use the built-in method on array to find the index, also write the gameplay rules

--- Memorize/Memorize/MemoryGame.swift
+++ Memorize/Memorize/MemoryGame.swift
@@ -20,20 +20,25 @@ struct MemoryGame<CardContent> where CardContent: Equatable {
         }
     }
     
-    // TODO: Implement game logic
+    // Gameplay rules: (All cards need to be face-down when the app starts)
+    // 1. When player chooses a first card, you should face it up.
+    // 2. When player chooses a second card, you have to make a match b/w the two face-up cards.
+    // 3. When player chooses a third card, the previous two-cards must be face down.
+    //
+    // Matching Rules: What to do in step 2 of the gameplay rules.
+    // a. If there's a match b/w the two face-up cards, you should face-them down, and preferrably remove/hide from the deck of cards.
+    // b. If there's no match, do nothing and go to step 3 when player chooses the next card.
+    //
+    // Some extra rules.
+    // - If there's a card that's already matched and the user taps it, do nothing.
+    // - Also, if the player taps the same card that's already face-up, do nothing.
+    //
+    // In a nutshell: you can only match a card with another card if there's only one extra face-up card.
+    //                - At one time, there can be only two cards that are face-up.
     mutating func choose(_ card: Card) {
-        let chosenIndex = index(of: card)
-        cards[chosenIndex].isFaceUp.toggle()
-    }
-    
-    private func index(of card: Card) -> Int {
-        for index in cards.indices {
-            if cards[index].id == card.id {
-                return index
-            }
+        if let chosenIndex = cards.firstIndex(of: card) {
+            cards[chosenIndex].isFaceUp.toggle()
         }
-        
-        return 0 // FIXME: bogus!
     }
     
     mutating func shuffle() {
